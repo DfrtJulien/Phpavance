43948c2049250a96276060d4449e949de9077543
DfrtJulien <de.freitas.julien78@gmail.com>
2024-09-09 14:41:48 +0200

    exercice sur GET fini 2

--- app/Views/get.view.php
+++ app/Views/get.view.php
@@ -1,10 +1,11 @@
 <?php
     include '../Views/partials/head.php';
 ?>
-    <a href="recupget.view.php?prenom=Abdou&age=20">Utilisateur</a>
-    <a href="recupget.view.php?article=jean&couleur=bleu&prix=30">Jean bleu</a>
-    <a href="recupget.view.php?article=robe&couleur=rouge&prix=30">Robe rouge</a>
-    <a href="recupget.view.php?article=pull&couleur=blanc&prix=30">Pull blanc</a>
+    <a href="traitementexo.view.php?prenom=Abdou&age=20">Utilisateur</a>
+    <a href="traitementexo.view.php?article=jean&couleur=bleu&prix=120">Jean bleu</a>
+    <a href="traitementexo.view.php?article=robe&couleur=rouge&prix=50">Robe rouge</a>
+    <a href="traitementexo.view.php?article=pull&couleur=blanc&prix=80">Pull blanc</a>
+    <a href="traitementexo.view.php?article=jogging&couleur=grix&prix=10">Jogging Grix</a>
 <?php
     include '../Views/partials/footer.php';
 ?>
